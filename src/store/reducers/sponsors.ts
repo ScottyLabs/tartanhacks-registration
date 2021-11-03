7cefed46dafa88bb0a13c36c0587f066336b12c0
Gram Liu <gram.liu226@gmail.com>
2021-11-03 09:02:58 -0400

    Added error reducer

--- src/store/reducers/sponsors.ts
+++ src/store/reducers/sponsors.ts
@@ -5,23 +5,34 @@ import { DispatchAction } from "types/DispatchAction"
 
 const data = (state = [], action: DispatchAction) => {
   switch (action.type) {
-  case DispatchActionType.SPONSORS_LIST:
-    if (action.status == RequestStatus.SUCCESS) {
-      return action.data
-    }
+    case DispatchActionType.SPONSORS_LIST:
+      if (action.status == RequestStatus.SUCCESS) {
+        return action.data
+      }
+  }
+  return state
+}
+
+const error = (state = {}, action: DispatchAction) => {
+  switch (action.type) {
+    case DispatchActionType.SPONSORS_LIST:
+      if (action.status == RequestStatus.ERROR) {
+        return action.data
+      }
   }
   return state
 }
 
 const status = (state = null, action: DispatchAction) => {
   switch (action.type) {
-  case DispatchActionType.SPONSORS_LIST:
-    return action.status
+    case DispatchActionType.SPONSORS_LIST:
+      return action.status
   }
   return state
 }
 
 export default combineReducers({
   data,
+  error,
   status
 })
