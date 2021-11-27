7484eddf39004a7c9d2b04faaaa9e87102701fec
Gram Liu <gram.liu226@gmail.com>
2021-11-27 02:31:22 -0500

    Linted code

--- src/components/form/ApplicationForm/index.tsx
+++ src/components/form/ApplicationForm/index.tsx
@@ -65,7 +65,7 @@ const useStyles = makeStyles((theme) => ({
     },
     [theme.breakpoints.down(theme.breakpoints.values.mobile)]: {
       fontSize: "1.8em"
-    },
+    }
   },
   formContents: {
     marginTop: "2em",
