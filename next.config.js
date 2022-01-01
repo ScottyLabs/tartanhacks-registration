7d17871c7a127b565d3b4e5e0f326e85cd9ba458
Gram Liu <gram.liu226@gmail.com>
2022-01-01 21:22:13 +0800

    Removed redux dev middleware from prod build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const isProduction = true
 module.exports = {
-  target: "serverless",
   poweredByHeader: false,
   reactStrictMode: false,
   env: {
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware, compose } from "redux"
 import reducers from "./reducers"
 import api from "./middleware/api"
-// import logger from "redux-logger"
 
 const middleware = [api /*logger*/]
 
 const enhancedCompose =
-  (typeof window !== "undefined" &&
+  (process.env.NODE_ENV === "development" && typeof window !== "undefined" &&
     (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose
 
