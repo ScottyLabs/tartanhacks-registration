40cfa01a5864bffdda09349887878254fc651ed8
David Ethan Hwang <hdavidethan@gmail.com>
2022-11-19 16:17:22 -0500

    Add Home page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { makeStyles } from "@material-ui/core"
 import { NextPage } from "next"
 import { ReactElement } from "react"
 import DashboardDialog from "src/components/dashboard/DashboardDialog"
@@ -6,31 +5,19 @@ import ScottyLabsHeader from "src/components/design/ScottyLabsHeader"
 import WaveBackground from "src/components/design/WaveBackground"
 import Menu from "src/components/menu/Menu"
 import { AuthenticatedLayout } from "src/layouts"
+import styles from "styles/Home.module.scss"
 
-const useStyles = makeStyles((theme) => ({
-  dialog: {
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-}))
-
-const Home: NextPage = (): ReactElement => {
-  const classes = useStyles()
-  return (
-    <>
-      <WaveBackground />
-      <Menu />
-      <div>
-        <ScottyLabsHeader />
-        <div className={classes.dialog}>
-          <DashboardDialog />
-        </div>
+const Home: NextPage = (): ReactElement => (
+  <>
+    <WaveBackground />
+    <Menu />
+    <div>
+      <ScottyLabsHeader />
+      <div className={styles.dialog}>
+        <DashboardDialog />
       </div>
-    </>
-  )
-}
+    </div>
+  </>
+)
 
 export default AuthenticatedLayout(Home)
