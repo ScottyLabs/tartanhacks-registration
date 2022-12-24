173ae82755ec863f9bd87618dc7b74de129a8d0e
Gram Liu <gram.liu226@gmail.com>
2022-12-24 11:51:19 +0800

    Migrate forgot-password page to SCSS

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -1,54 +1,18 @@
-import { makeStyles } from "@material-ui/core"
 import { NextPage } from "next"
-import React, { ReactElement, useEffect } from "react"
+import { ReactElement } from "react"
 import PasswordResetDialog from "src/components/auth/RequestResetDialog"
 import ScottyLabsIcon from "src/components/design/ScottyLabsIcon"
 import WaveHeader from "src/components/design/WaveHeader"
-
-const useStyles = makeStyles((theme) => ({
-  dialog: {
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: "10em",
-    boxSizing: "border-box",
-    [theme.breakpoints.down(theme.breakpoints.values.tablet)]: {
-      paddingTop: "3em"
-    }
-  },
-  scottyContainer: {
-    zIndex: -1,
-    opacity: 0.3,
-    bottom: 0,
-    width: "100%",
-    height: "100%",
-    position: "absolute",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "end"
-  },
-  scottyIcon: {
-    position: "relative",
-    width: "40%",
-    bottom: 0,
-    [theme.breakpoints.down(theme.breakpoints.values.tablet)]: {
-      width: "100%"
-    }
-  }
-}))
+import styles from "../styles/ForgotPassword.module.scss"
 
 const ForgotPasswordPage: NextPage = (): ReactElement => {
-  const classes = useStyles()
-
   return (
     <div>
       <WaveHeader />
-      <div className={classes.scottyContainer}>
-        <ScottyLabsIcon className={classes.scottyIcon} />
+      <div className={styles.scottyContainer}>
+        <ScottyLabsIcon className={styles.scottyIcon} />
       </div>
-      <div className={classes.dialog}>
+      <div className={styles.dialog}>
         <PasswordResetDialog />
       </div>
     </div>
