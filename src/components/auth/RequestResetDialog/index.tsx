6ecfa266cf293c2b995b19015effb9310641e3fc
Gram Liu <gram.liu226@gmail.com>
2022-12-24 13:21:52 +0800

    Migrate forgot-password page to SCSS (#136)
    * Migrate forgot-password page to SCSS
    
    * Migrate RequestResetDialog to SCSS
    
    * Update RequestReset component to use SCSS module
    
    * Migrate PasswordResetDialog to SCSS
    
    * Fix header background gradients

--- src/components/auth/RequestResetDialog/index.tsx
+++ src/components/auth/RequestResetDialog/index.tsx
@@ -1,65 +1,20 @@
 import {
   Collapse,
   LinearProgress,
-  makeStyles,
   Snackbar,
   TextField,
   Typography
 } from "@material-ui/core"
 import { Alert } from "@material-ui/lab"
-import { useTheme } from "@material-ui/styles"
-import { useRouter } from "next/dist/client/router"
 import { ReactElement, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import actions from "src/actions"
 import { RootState } from "types/RootState"
-import RectangleButton from "../design/RectangleButton"
-
-const useStyles = makeStyles((theme) => ({
-  dialog: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "25%",
-    padding: "1em",
-    textAlign: "center",
-    [theme.breakpoints.down(theme.breakpoints.values.tablet)]: {
-      width: "50%"
-    },
-    [theme.breakpoints.down(theme.breakpoints.values.mobile)]: {
-      width: "80%"
-    }
-  },
-  resetForm: {
-    display: "flex",
-    flexDirection: "column",
-    gap: "1em",
-    width: "100%"
-  },
-  header: {
-    fontWeight: 600,
-    backgroundImage: `linear-gradient(180deg, ${theme.palette.gradient.start} 19.64%, ${theme.palette.gradient.end} 69.64%)`,
-    backgroundClip: "text",
-    WebkitBackgroundClip: "text",
-    WebkitTextFillColor: "transparent",
-    marginBottom: "1em"
-  },
-  dialogContent: {
-    display: "flex",
-    flexDirection: "column",
-    marginTop: "-10em",
-    color: theme.palette.gradient.start
-  },
-  emailLabel: {
-    fontWeight: "bold"
-  }
-}))
+import RectangleButton from "../../design/RectangleButton"
+import styles from "./index.module.scss"
 
 const RequestResetDialog = (): ReactElement => {
   const dispatch = useDispatch()
-  const theme = useTheme()
-  const router = useRouter()
-  const classes = useStyles(theme)
   const [email, setEmail] = useState("")
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
@@ -81,7 +36,7 @@ const RequestResetDialog = (): ReactElement => {
 
   const resetForm = (
     <form
-      className={classes.resetForm}
+      className={styles.resetForm}
       onSubmit={(e) => {
         e.preventDefault()
 
@@ -108,7 +63,7 @@ const RequestResetDialog = (): ReactElement => {
     <>
       <Typography variant="body1">
         A password reset email has been sent to
-        <span className={classes.emailLabel}> {email}</span>.
+        <span className={styles.emailLabel}> {email}</span>.
       </Typography>
       <Typography variant="body1">
         Click the link in your email to proceed.
@@ -117,7 +72,7 @@ const RequestResetDialog = (): ReactElement => {
   )
 
   return (
-    <div className={classes.dialog}>
+    <div className={styles.dialog}>
       <Collapse in={loading}>
         <LinearProgress />
       </Collapse>
@@ -129,8 +84,8 @@ const RequestResetDialog = (): ReactElement => {
       >
         <Alert severity="error">{errorMessage}</Alert>
       </Snackbar>
-      <div className={classes.dialogContent}>
-        <Typography variant="h4" className={classes.header}>
+      <div className={styles.dialogContent}>
+        <Typography variant="h4" className={styles.header}>
           Account Recovery
         </Typography>
         {sentEmail ? sentEmailMessage : resetForm}
