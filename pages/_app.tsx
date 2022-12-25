be7e407a86dd00a79ba3d823737f0d0ee0e3fee8
Gram Liu <gram.liu226@gmail.com>
2022-12-25 21:45:42 +0800

    Replace scripts in _app with NextScript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app"
 import Head from "next/head"
 import { ReactElement } from "react"
 import { Provider } from "react-redux"
+import Script from "next/script"
 import store from "src/store"
 import { theme } from "src/themes/theme"
 import "styles/globals.scss"
@@ -14,22 +15,20 @@ const App = ({ Component, pageProps }: AppProps): ReactElement => {
         <title>TartanHacks</title>
         <meta name="description" content="Register for TartanHacks" />
         <link rel="icon" href="/favicon.ico" />
-        <script
+        <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-GDT87TLX5E"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-GDT87TLX5E', {
-              page_path: window.location.pathname,
-            });
-          `
-          }}
-        />
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
+            page_path: window.location.pathname,
+          });
+          `}
+        </Script>
       </Head>
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={theme}>
