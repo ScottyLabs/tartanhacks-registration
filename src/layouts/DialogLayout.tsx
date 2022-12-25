753f456955114b19651f176b31ebbf1ccb75f0f2
Gram Liu <gram.liu226@gmail.com>
2022-12-25 18:27:12 +0800

    Migrate layouts to SCSS (#142)
    * Migrate layouts to SCSS
    
    * Add missing styles import to AdminLayout

--- src/layouts/DialogLayout.tsx
+++ src/layouts/DialogLayout.tsx
@@ -1,25 +1,13 @@
-import React, { ReactElement, FunctionComponent } from "react"
-import { makeStyles } from "@material-ui/core/styles"
-
-const useStyles = makeStyles(() => ({
-  root: {
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-}))
+import { FunctionComponent, ReactElement } from "react"
+import styles from "./index.module.scss"
 
 /**
  * A basic centered layout containing a dialog centered to the middle of the page
  */
 const DialogLayout = (Page: FunctionComponent) => (): ReactElement => {
-  const classes = useStyles({})
-
   return (
     <>
-      <div className={classes.root}>
+      <div className={styles.dialog}>
         <Page />
       </div>
     </>
