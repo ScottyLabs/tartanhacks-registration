c99325900fb615fdc8035679c1f9455b5a96e5d4
Gram Liu <gram.liu226@gmail.com>
2022-12-25 18:08:09 +0800

    Add missing RectangleButton and WaveFooter components

---
+++ src/components/design/RectangleButton/index.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@material-ui/core"
+import React, { ReactElement } from "react"
+import styles from "./index.module.scss"
+
+const RectangleButton = ({
+  className,
+  children,
+  type,
+  onClick,
+  startIcon,
+  backgroundColor
+}: {
+  className?: string
+  children?: ReactElement | string
+  type: "button" | "reset" | "submit" | undefined
+  onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined
+  startIcon?: ReactElement
+  backgroundColor?: string
+}): ReactElement => {
+  return (
+    <>
+      <Button
+        variant="contained"
+        type={type}
+        className={`${className} ${styles.button}`}
+        onClick={onClick}
+        startIcon={startIcon}
+        style={{ backgroundColor }}
+      >
+        {children}
+      </Button>
+    </>
+  )
+}
+
+export default RectangleButton
