b5b61fa72d7c9ae50020dcf5f15161bf2d0b517b
Gram Liu <gram.liu226@gmail.com>
2022-12-25 17:51:30 +0800

    Migrate ContentHeader to SCSS

---
+++ src/components/design/ContentHeader/index.tsx
@@ -0,0 +1,23 @@
+import { Hidden, Typography } from "@material-ui/core"
+import styles from "./index.module.scss"
+
+const ContentHeader = (props: any) => {
+  return (
+    <>
+      <div className={styles.headerDiv}>
+        <Typography
+          variant="h4"
+          className={`${styles.header} 
+          ${props.longTitle ? styles.longTitle : ""}`}
+        >
+          {props.title}
+        </Typography>
+      </div>
+      <Hidden xsDown>
+        <hr className={styles.hrDivider} />
+      </Hidden>
+    </>
+  )
+}
+
+export default ContentHeader
