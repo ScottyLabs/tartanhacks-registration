ae0d45438d9964f1097dfb8ac022d2cae716936b
Gram Liu <gram.liu226@gmail.com>
2022-12-26 17:15:50 +0800

    Disable cache control

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,13 @@ const App = ({ Component, pageProps }: AppProps): ReactElement => {
     <>
       <Head>
         <title>TartanHacks</title>
+        {/* Disable cache control */}
+        <meta
+          httpEquiv="Cache-Control"
+          content="no-cache, no-store, must-revalidate"
+        />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
         <meta name="description" content="Register for TartanHacks" />
         <link rel="icon" href="/favicon.ico" />
         <Script
