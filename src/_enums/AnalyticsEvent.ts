25540e86fd437f068fb3183e66d78277c2affe44
David Ethan Hwang <hdavidethan@gmail.com>
2023-01-01 22:05:49 +0800

    Add email verification to analytics

--- src/_enums/AnalyticsEvent.ts
+++ src/_enums/AnalyticsEvent.ts
@@ -1,3 +1,6 @@
 export default {
-  ACCOUNT_CREATED: "account_created"
+  ACCOUNT_CREATED: "account_created",
+  EMAIL_VERIFIED: "email_verified",
+  APPLICATION_SUBMITTED: "application_submitted",
+  ATTENDANCE_CONFIRMED: "attendance_confirmed"
 } as const
